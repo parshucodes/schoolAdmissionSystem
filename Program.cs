f5977290c94b642481227536dac50b577cd1cab6
parshucodes <parshuram14234@gmail.com>
2024-02-17 00:03:28 +0530

    new commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace SchoolAdmission;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        School anjuman = new School();
+        Student child1 = new Student("Akshay", 10, "5th Grade");
+        Student child2 = new Student("Pujara", 9, "4th Grade");
+        Student child3 = new Student("Chetana", 11, "6th Grade");
+
+        anjuman.AddInformation(child1);
+        anjuman.AddInformation(child2);
+        anjuman.AddInformation(child3);
+
+        anjuman.DisplayStidentInformation();
+    }
+}
---
+++ schooladmission.cs
@@ -0,0 +1,36 @@
+﻿namespace SchoolAdmission;
+
+class Student
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Grade { get; set; }
+
+    public Student(string name, int age, string grade)
+    {
+        Name = name;
+        Age = age;
+        Grade = grade;
+    }
+}
+class School
+{
+    private List<Student> admission;
+    public School()
+    {
+        admission = new List<Student>();
+    }
+
+    public void AddInformation(Student student)
+    {
+        admission.Add(student);
+    }
+
+    public void DisplayStidentInformation()
+    {
+        foreach (var item in admission)
+        {
+            Console.WriteLine($"Student Name:{item.Name} \nAge: {item.Age} \nGrade: {item.Grade} \n");
+        }
+    }
+}
